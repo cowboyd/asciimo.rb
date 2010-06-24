8d4cc76d4047c4626b9934918a1c81142edf2130
Charles Lowell <cowboyd@thefrontside.net>
2010-06-24 14:09:47 +0300

    0.2.0: Upgrade to work with the OSS clean version of asciimo.

--- lib/asciimo.rb
+++ lib/asciimo.rb
@@ -2,25 +2,29 @@ require 'v8'
 
 module Asciimo
   VERSION = "0.2.0"
-  HOME = File.join(File.dirname(__FILE__), "..", "ext", "asciimo")
+  HOME = File.expand_path(File.join(File.dirname(__FILE__), "..", "ext", "asciimo"))
   LIB = File.join(HOME, "lib")
   FONTS = File.join(HOME, "fonts")
   
-  def text(font_name, text, color = nil)
+  def text(font, text, color = nil)
     cxt = V8::Context.new
     asciimo = cxt.scope
+    asciimo['window'] = asciimo
+    asciimo['$'] = JQuery.new
     cxt.load("#{LIB}/asciimo.js")
     cxt.load("#{LIB}/colors.js")
-    font = File.read("#{FONTS}/#{font_name}")
-    asciimo.loadFont(font)
-    art = asciimo.createLargeText(text)
-    return color ? asciimo.stylize(art, color) : art
+
+    ascii = nil
+    asciimo.Figlet.write(text, font, proc {|result| ascii = result})
+
+    return color ? asciimo.stylize(ascii, color) : ascii
   end
 
   def fonts
     cxt = V8::Context.new
+    figlet = cxt['Figlet'] = cxt['Object'].new
     cxt.load("#{LIB}/fonts.js")
-    cxt.scope.asciimoFonts.to_a
+    figlet.fontList.to_a
   end
 
   def colors
@@ -42,5 +46,11 @@ module Asciimo
   end
 
   module_function :text, :colors, :fonts
+  
+  class JQuery
+    def get(file, callback)
+      callback.call(File.read File.join(Asciimo::HOME, file))
+    end
+  end
 end
 
